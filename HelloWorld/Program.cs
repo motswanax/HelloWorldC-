611705bf53415fbb14d78a46daa8558640a39e27
Baiketli Dijo <bdijo@botswanapost.co.bw>
2020-10-28 12:16:29 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What month were you born in?");
+            var birthMonth = Console.ReadLine();
+         
+            Console.WriteLine($"Hi {name}. You are {age} years old and born in {birthMonth}");
+
+            if(birthMonth == "march")
+            {
+                Console.WriteLine("You are an Aries");
+            } else if(birthMonth == "april")
+            {
+                Console.WriteLine("You are a Taurus");
+            }
+            else if (birthMonth == "may")
+            {
+                Console.WriteLine("You are a Gemini");
+            }
+        }
+    }
+}
