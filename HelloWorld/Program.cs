41af3dde349da36d84c0d842e3136662a22ce051
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-29 14:26:50 +0200

    demo of reference versus value type

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,26 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            var age = Console.ReadLine();
-            Console.WriteLine("What month were you born in?");
-            var birthMonth = Console.ReadLine();
-         
-            Console.WriteLine($"Hi {name}. You are {age} years old and born in {birthMonth}");
+            /* This prints 2 5 2. The x is not referenced by Five() so it is not changed. */
+            var x = 2;
+            Console.WriteLine(x);
 
-            if(birthMonth == "march")
-            {
-                Console.WriteLine("You are an Aries");
-            } else if(birthMonth == "april")
-            {
-                Console.WriteLine("You are a Taurus");
-            }
-            else if (birthMonth == "may")
-            {
-                Console.WriteLine("You are a Gemini");
-            }
+            Five(x);
+
+            Console.WriteLine(x);
+        }
+
+        static void Five(int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
         }
     }
 }
