7fb07137de53edfb13d99664ea2573802478a92d
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-29 14:30:00 +0200

    using out to reference values passed in as parameters

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,16 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            /* This prints 2 5 2. The x is not referenced by Five() so it is not changed. */
+            /* This prints 2 5 5 because of 'out'. The x is not referenced by Five() so it is not changed. */
             var x = 2;
             Console.WriteLine(x);
 
-            Five(x);
+            /* Out lets us reference the value that is passed in. */
+            Five(out x);
 
             Console.WriteLine(x);
         }
 
-        static void Five(int a)
+        /* Out lets us reference the value that is passed in. */
+        static void Five(out int a)
         {
             a = 5;
             Console.WriteLine(a);
