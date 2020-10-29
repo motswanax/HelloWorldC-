97a1b18ae6707bc3a5cb2b8adbea7b0c53d996ed
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-29 16:11:18 +0200

    used abstract classes in the calculation of shape area

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,26 +6,42 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            var age = Console.ReadLine();
-            Console.WriteLine("What month were you born in?");
-            var birthMonth = Console.ReadLine();
-         
-            Console.WriteLine($"Hi {name}. You are {age} years old and born in {birthMonth}");
+            var square = new Square() { Width = 2 };
+            var triangle = new Triangle() { Base = 2, Height = 5 };
 
-            if(birthMonth == "march")
-            {
-                Console.WriteLine("You are an Aries");
-            } else if(birthMonth == "april")
-            {
-                Console.WriteLine("You are a Taurus");
-            }
-            else if (birthMonth == "may")
-            {
-                Console.WriteLine("You are a Gemini");
-            }
+            square.Display();
+            triangle.Display();
+        }
+    }
+
+    abstract class Shape
+    {
+        public abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("The area is {0}", GetArea());
+        }
+    }
+
+    class Square : Shape
+    {
+        public int Width;
+
+        public override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+
+        public override int GetArea()
+        {
+            return (Base * Height) / 2;
         }
     }
 }
