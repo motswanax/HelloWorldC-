6589ad8a11f7eaa9fed9dfcccd805f733329ef95
bdijo <bdijo@D-IMEC-TRAINEE->
2020-10-29 14:39:23 +0200

    using the ref keyword to reference parameters passed in and change their value

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,20 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            /* This prints 2 5 5 because of 'out'. The x is not referenced by Five() so it is not changed. */
+            /* This prints 2 4 4 because of 'ref'. The x is referenced by Five() so it is changed. */
             var x = 2;
             Console.WriteLine(x);
 
-            /* Out lets us reference the value that is passed in. */
-            Five(out x);
+            /* ref lets us reference the value that is passed in. */
+            Double(ref x);
 
             Console.WriteLine(x);
         }
 
         /* Out lets us reference the value that is passed in. */
-        static void Five(out int a)
+        static void Five(ref int a)
         {
             a = 5;
+            Console.WriteLine(a);
+        }
+
+        static void Double(ref int a)
+        {
+            a = a * 2;
             Console.WriteLine(a);
         }
     }
